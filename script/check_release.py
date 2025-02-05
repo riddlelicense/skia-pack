57791c3c1ebe7a6a3ba599817d2fcdae96916f2f
prxd <32244379+riddlelicense@users.noreply.github.com>
2025-02-05 17:11:27 +0500

    Update check_release.py

--- script/check_release.py
+++ script/check_release.py
@@ -3,23 +3,7 @@
 import common, json, sys, urllib.request
 
 def main():
-  headers = common.github_headers()
-  version = common.version()
-  build_type = common.build_type()
-  target = common.target()
-  machine = common.machine()
-  classifier = common.classifier()
-
-  try:
-    resp = urllib.request.urlopen(urllib.request.Request('https://api.github.com/repos/JetBrains/skia-pack/releases/tags/' + version, headers=headers)).read()
-    artifacts = [x['name'] for x in json.loads(resp.decode('utf-8'))['assets']]
-    zip = 'Skia-' + version + '-' + target + '-' + build_type + '-' + machine + classifier + '.zip'
-    if zip in artifacts:
-      print('> Artifact "' + zip + '" exists, stopping')
-      return 1
-    return 0
-  except urllib.error.URLError as e:
-    return 0
+  return 0
 
 if __name__ == '__main__':
   sys.exit(main())
